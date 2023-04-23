4b8b9776a7f2d386e9c30687ab489e5b40317415
Omar Swailam <omarswailam@outlook.com>
2023-04-23 21:38:18 +0200

    serving media files ready

--- myrecipes/settings.py
+++ myrecipes/settings.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import os
 from pathlib import Path
 import environ
 
@@ -132,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- myrecipes/urls.py
+++ myrecipes/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -23,5 +25,7 @@ urlpatterns = [
         name="redoc",
     ),
     path("recipes/", include("recipes.urls")),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
