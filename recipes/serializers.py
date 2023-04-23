305289f1f6ce8b84d466c67cf769f3dd0e8d6864
Omar Swailam <omarswailam@outlook.com>
2023-04-23 02:30:29 +0200

    create, list and retrive tags as list

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from rest_framework import serializers
 from .models import Recipe, Tag
 from django.contrib.auth import get_user_model
@@ -17,8 +18,26 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ["id", "name"]
 
 
+class TagsSerializerField(serializers.ListField):
+    child = serializers.CharField()
+
+    def to_representation(self, data):
+        if isinstance(data, QuerySet):
+            # when you retrieve all recipe objects and prefetch their tags field using prefetch_related,
+            # you get a queryset that includes the related Tag objects for each Recipe object
+            # therefore, you can access the tag names directly from each Tag object in the queryset.
+            tags = [tag.name for tag in data]
+        else:
+            # when you retrieve a single recipe object and access its tags field,
+            # you get a related manager object because the tags field is a many-to-many relationship
+            # therefore, you need to use the all() method on the related manager object to retrieve
+            # the related Tag objects and access their names.
+            tags = [tag.name for tag in data.all()]
+        return tags
+
+
 class RecipeSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True, required=False)
+    tags = TagsSerializerField(required=False)
 
     class Meta:
         model = Recipe
@@ -38,7 +57,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         recipe = super(RecipeSerializer, self).create(validated_data)
 
         # Create a list of Tag objects to be bulk created
-        tag_objs = [Tag(**tag) for tag in tags]
+        tag_objs = [Tag(name=tag) for tag in tags]
 
         # Bulk create Tag objects and get their IDs
         created_tags = Tag.objects.bulk_create(tag_objs)
@@ -52,7 +71,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         tags = validated_data.pop("tags", None)
         recipe = super(RecipeSerializer, self).update(instance, validated_data)
 
-        tag_objs = [Tag(**tag) for tag in tags]
+        tag_objs = [Tag(name=tag) for tag in tags]
 
         created_tags = Tag.objects.bulk_create(tag_objs)
 
