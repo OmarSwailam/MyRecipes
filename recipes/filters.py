eaabdfaee5517fb29d5157f2a0af3d10627b3be4
Omar Swailam <omarswailam@outlook.com>
2023-04-23 20:50:51 +0200

    created custom filter for recipe to filter by tags, ingredients

--- recipes/filters.py
+++ recipes/filters.py
@@ -3,8 +3,14 @@ from rest_framework.filters import BaseFilterBackend
 
 class RecipeFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        if "me" in request.query_params:
-            me = request.query_params.get("me")
+        tags = request.query_params.getlist("tags", None)
+        if tags:
+            queryset = queryset.filter(tags__name__in=tags).distinct()
+        ingredients = request.query_params.getlist("ingredients", None)
+        if ingredients:
+            queryset = queryset.filter(ingredients__name__in=ingredients).distinct()
+        me = request.query_params.get("me")
+        if me:
             if me.lower() == "true":
                 queryset = queryset.filter(user_id=request.user)
             elif me.lower() == "false":
