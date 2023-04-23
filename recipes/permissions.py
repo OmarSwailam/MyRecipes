f321c237c9d38ed9628ec0f8b31442e3a25bdd5f
Omar Swailam <omarswailam@outlook.com>
2023-04-23 21:13:29 +0200

    modify permission to allow only activated accounts to post, and allow admins to edit or delete public recipes

--- recipes/permissions.py
+++ recipes/permissions.py
@@ -9,7 +9,7 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        return request.user and request.user.is_authenticated
+        return request.user and request.user.is_authenticated and request.user.is_active
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
