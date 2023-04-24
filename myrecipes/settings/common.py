80897128fc7adca5aa129231e5a5ae9a8ddfe904
Omar Swailam <omarswailam@outlook.com>
2023-04-25 00:06:32 +0200

    default pagination class

--- myrecipes/settings/common.py
+++ myrecipes/settings/common.py
@@ -117,6 +117,8 @@ CACHES = {
 }
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 20,
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
